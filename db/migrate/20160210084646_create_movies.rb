a60aa6c39c78eeeddc81250e0594cf37a052c15d
Mahendhar <mahendarreddy@live.com>
2016-02-10 14:25:07 +0530

    Scaffolded movies

---
+++ db/migrate/20160210084646_create_movies.rb
@@ -0,0 +1,18 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :hotness
+      t.string :image_url
+      t.text :synopsis
+      t.string :rating
+      t.string :genre
+      t.string :director
+      t.date :release_date
+      t.string :runtime
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
